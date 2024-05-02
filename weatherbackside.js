775fc7e6efbe862c0720bd67d75d965a293cff4f
ElhaanDaud <elhaan.daud786@gmail.com>
2024-05-03 00:33:29 +0530

    try n catch

--- weatherbackside.js
+++ weatherbackside.js
@@ -10,29 +10,33 @@ const weatherIcon= document.querySelector(".weather-icon");
 
 
 async function checkWeather(city){
-    const response= await fetch(apiURL + city + `&appid=${apiKey}`);
-    var data = await response.json();
+    try{
+        const response= await fetch(apiURL + city + `&appid=${apiKey}`);
+        var data = await response.json();
 
-    console.log(data);
-    document.querySelector(".city").innerHTML= data.name;
-    document.querySelector(".temp").innerHTML= parseInt(data.main.temp) + '°c';
-    document.querySelector(".humidity").innerHTML= data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML= Math.ceil(data.wind.speed) + 'km/h';
-    
-    if(data.weather[0].main=="Clouds"){
+        console.log(data);
+        document.querySelector(".city").innerHTML= data.name;
+        document.querySelector(".temp").innerHTML= parseInt(data.main.temp) + '°c';
+        document.querySelector(".humidity").innerHTML= data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML= Math.ceil(data.wind.speed) + ' km/h';
+
+        if(data.weather[0].main=="Clouds"){
         weatherIcon.src= "images/clouds.png";
-    }
-    else if(data.weather[0].main=="Clear"){
+        }
+        else if(data.weather[0].main=="Clear"){
         weatherIcon.src= "images/clear.png";
-    }
-    else if(data.weather[0].main=="Rain"){
+        }
+        else if(data.weather[0].main=="Rain"){
         weatherIcon.src= "images/rain.png";
-    }
-    else if(data.weather[0].main=="Drizzle"){
+        }
+        else if(data.weather[0].main=="Drizzle"){
         weatherIcon.src= "images/drizzle.png";
-    }
-    else if(data.weather[0].main=="Mist"){
+        }
+        else if(data.weather[0].main=="Mist"){
         weatherIcon.src= "images/mist.png";
+        }
+    }catch (error) {
+        console.error("Error fetching weather data:", error);
     }
     
 }
